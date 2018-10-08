d2c06296d88465e197d44d0c2faddb1b8dc3162c
JonniPolar <jonj16@ru.is>
2018-10-08 14:52:18 +0000

    change function to take one number and return it

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require("./StringCalculator");
 it("Should return zero on an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+it("Should return number if only one number is inserted", () => {
+    expect(add("5")).toBe(5);
+});
